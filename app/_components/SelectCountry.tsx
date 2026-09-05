bed8d7b26f9dd93539cb50bdf34462ff3617eaef
abdelrahmantaymour <abdalrahman.m.taymor@gmail.com>
2026-09-05 14:35:32 +0300

    feat: Add SelectCountry component for country selection with flags

---
+++ app/_components/SelectCountry.tsx
@@ -0,0 +1,39 @@
+import { getCountries } from "@/app/_lib/data-service";
+
+type SelectCountryProps = {
+  defaultCountry: string;
+  name: string;
+  id: string;
+  className?: string;
+};
+
+async function SelectCountry({
+  defaultCountry,
+  name,
+  id,
+  className,
+}: SelectCountryProps) {
+  const countries = await getCountries();
+
+  const flag =
+    countries.find((country) => country.name === defaultCountry)?.flag ?? "";
+
+  return (
+    <select
+      name={name}
+      id={id}
+      defaultValue={`${defaultCountry}%${flag}`}
+      className={className}
+    >
+      <option value="">Select country...</option>
+
+      {countries.map((country) => (
+        <option key={country.name} value={`${country.name}%${country.flag}`}>
+          {country.name}
+        </option>
+      ))}
+    </select>
+  );
+}
+
+export default SelectCountry;
