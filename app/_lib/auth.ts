95e718d6c74618f00ab9ab70e1e8f91a09daee98
abdelrahmantaymour <abdalrahman.m.taymor@gmail.com>
2026-09-17 16:50:47 +0300

    feat: Implement SignInButton component and integrate sign-in functionality with Google

--- app/_lib/auth.ts
+++ app/_lib/auth.ts
@@ -13,4 +13,12 @@ export const {
       clientSecret: process.env.AUTH_GOOGLE_SECRET,
     }),
   ],
+  callbacks: {
+    authorized({ auth, request }) {
+      return !!auth?.user;
+    },
+  },
+  pages: {
+    signIn: "/login",
+  },
 });
