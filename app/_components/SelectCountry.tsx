a63d4184dab84a9f70764c437661462a25b48dc6
abdelrahmantaymour <abdalrahman.m.taymor@gmail.com>
2026-09-22 13:06:31 +0300

    feat: Update SelectCountry component to use concise variable naming and improve code readability; add revalidation in updateGuest action

--- app/_components/SelectCountry.tsx
+++ app/_components/SelectCountry.tsx
@@ -22,14 +22,15 @@ async function SelectCountry({
     <select
       name={name}
       id={id}
+      // Here we use a trick to encode BOTH the country name and the flag into the value. Then we split them up again later in the server action
       defaultValue={`${defaultCountry}%${flag}`}
       className={className}
     >
       <option value="">Select country...</option>
 
-      {countries.map((country) => (
-        <option key={country.name} value={`${country.name}%${country.flag}`}>
-          {country.name}
+      {countries.map((c) => (
+        <option key={c.name} value={`${c.name}%${c.flag}`}>
+          {c.name}
         </option>
       ))}
     </select>
